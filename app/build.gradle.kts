5df443f816f8c509f1cc82e49e161e3b4a33b1df
Sbiswas001 <sbiswas001.tech@gmail.com>
2024-05-27 13:37:24 +0530

    minor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "sayan.apps.numplex"
-        minSdk = 31
+        minSdk = 30
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
